83624ec65f1defa0963e70e98c2c4881004d2d20
Mahmoud Abdullah Anani <99493135+MahmoudAbdullahAnani@users.noreply.github.com>
2024-04-28 06:36:47 +0300

    handle hero section

---
+++ components/Benefits/SingleBenefit.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+
+export default function SingleBenefit({ item }: any) {
+  return (
+    <div className={`z-1 relative max-h-[270px] max-w-[200px]`}>
+      <div className={`mx-auto flex flex-col items-center justify-center`}>
+        <img
+          src={item.image}
+          alt={item.title}
+          className={`h-full w-full`}
+          width={100}
+          height={100}
+        />
+        <p
+          className={`mt-[13px] text-center text-2xl font-extrabold text-[#117C99]`}
+        >
+          {item.text}
+        </p>
+      </div>
+      <div className={`absolute bottom-0 -left-5 z-[-1] h-[238px] w-[232px]`}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="232"
+          height="238"
+          viewBox="0 0 232 238"
+          fill="none"
+        >
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M115.08 0.0226046C147.956 -0.883594 173.809 25.6196 195.735 50.2122C216.162 73.123 232.812 100.4 231.969 131.127C231.146 161.166 213.564 187.478 191.418 207.711C170.394 226.92 143.493 239.091 115.08 237.923C87.706 236.797 64.8603 220.35 44.7526 201.681C23.5594 182.005 1.23501 160.072 0.0519584 131.127C-1.16308 101.4 19.1438 76.8729 38.7385 54.5396C60.1931 30.0866 82.6207 0.917312 115.08 0.0226046Z"
+            fill="#C6E9F7"
+          />
+        </svg>
+      </div>
+    </div>
+  );
+}
--- components/Benefits/index.tsx
+++ components/Benefits/index.tsx
@@ -1,5 +1,22 @@
-import Customers from "../Customers";
+import SingleBenefit from "./SingleBenefit";
 
+const data = [
+  {
+    image: "/images/Benefits/Benefits-1.png",
+    text: `أجواء تساعدك علي 
+تقليل التشتت`,
+  },
+  {
+    image: "/images/Benefits/Benefits-2.png",
+    text: `نظام نقاط مميز 
+من اجلك`,
+  },
+  {
+    image: "/images/Benefits/Benefits-3.png",
+    text: `تجربة استخدام
+ فرية`,
+  },
+];
 export default function Benefits() {
   return (
     <section className={`pt-16 text-center md:pt-20 lg:pt-28`}>
@@ -9,12 +26,18 @@ export default function Benefits() {
         <h1 className={`text-3xl font-extrabold text-primary `}>
           الفوائد ممكن التمتع بها
         </h1>
-        <p className={`text-[#117C99] text-center mt-[13px]`}>
+        <p className={`mt-[13px] max-w-[568px] text-center text-[#117C99]`}>
           بإمكانك الأن إضافة الأصدقاء أو إضافة أفراد العالم والإستمتاع بأفضل
           الأوقات المرحة, بدلاً من تضيع الوقت لتسجيل بيانات كل احد علي حدة الأن
           بضغطة زر فقط اضف عدد لامحدود من من تحب
         </p>
-        <div className={`flex max-w-[400px] overflow-x-scroll`}></div>
+        <div
+          className={`gap-[100px] mt-[87px] grid grid-cols-1 justify-center items-end sm:grid-cols-2 lg:grid-cols-3`}
+        >
+          {data.map((item) => (
+            <SingleBenefit key={item.image} item={item} />
+          ))}
+        </div>
       </div>
     </section>
   );
