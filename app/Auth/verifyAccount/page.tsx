dfb7732ff205e952544cdee10bd6c9d3e2f1b472
Mahmoud Abdullah Anani <99493135+MahmoudAbdullahAnani@users.noreply.github.com>
2024-06-18 14:09:40 +0300

    handle error ts v2

--- app/Auth/verifyAccount/page.tsx
+++ app/Auth/verifyAccount/page.tsx
@@ -1,19 +1,19 @@
-import React from "react";
+import React, { Suspense } from "react";
 import FormVerifyAccount from "./FormVerifyAccount";
-
 
 export const metadata = {
   title: "تأكيد الحساب || Ajwaa || أجواء ",
 };
 
 function page() {
-
   return (
     <section
       className="relative z-10 overflow-hidden pt-36 pb-16 md:pb-20 lg:pt-[180px] lg:pb-28"
       dir="rtl"
     >
-    <FormVerifyAccount />
+      <Suspense fallback={null}>
+        <FormVerifyAccount />
+      </Suspense>
     </section>
   );
 }
